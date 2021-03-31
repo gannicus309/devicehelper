cf5045c5ff0b390b0278d63df81cf893345aad4a
Ganesh Rengasamy <warrior.rons@gmail.com>
2021-03-31 18:18:27 +0200

    add logging and CMake run

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <exception>
 #include "run.hpp"
 #include <memory>
+#include "log.hpp"
 
 int main()
 {
     try {
+        LOG_GENERAL << "DeviceHelper starting.\n";
         devicehelper::run();
     } catch (std::exception& e) {
+        LOG_GENERAL << "DeviceHelper: unexpected exception: " << e.what() <<"\n";
         return 1;
     }
-
+    LOG_GENERAL << "DeviceHelper: Exiting application.\n";
     return 0;
 }

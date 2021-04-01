dffc7b4d1bd931d2f03c1877b118086bc4e4e99a
Ganesh Rengasamy <warrior.rons@gmail.com>
2021-04-01 17:55:48 +0200

    add devicehelper logic for AA detection

--- src/utils/AAPConsts.hpp
+++ src/utils/AAPConsts.hpp
@@ -3,35 +3,41 @@
  *  @author       Ganesh Rengasamy
  *********************************************************************/
 
- #pragma once
+#pragma once
 
-const int AOA_CLASS = 255;
-const int AOA_SUBCLASS = 255;
-const int AOA_PROTOCOL = 0;
+extern const int AOA_CLASS;
+extern const int AOA_SUBCLASS;
+extern const int AOA_PROTOCOL;
 
-const int AOAP_PRODUCT_MIN = 0x2d00;
-const int AOAP_PRODUCT_MAX = 0x2d05;
+extern const int AOAP_PRODUCT_MIN;
+extern const int AOAP_PRODUCT_MAX;
 
+#ifndef GOOG_VENDOR
 #define GOOG_VENDOR 0x18d1
+#endif
+#ifndef AOAP_PRODUCT_1
 #define AOAP_PRODUCT_1 0x2d00
+#endif
+#ifndef AOAP_PRODUCT_2
 #define AOAP_PRODUCT_2 0x2d01
+#endif
 
-const int ACCESSORY_STRING_MANUFACTURER = 0;
-const int ACCESSORY_STRING_MODEL = 1;
-const int ACCESSORY_STRING_DESCRIPTION = 2;
-const int ACCESSORY_STRING_VERSION = 3;
-const int ACCESSORY_STRING_URI = 4;
-const int ACCESSORY_STRING_SERIAL = 5;
-const int ACCESSORY_GET_PROTOCOL = 51;
-const int ACCESSORY_SEND_STRING = 52;
-const int ACCESSORY_START = 53;
+extern const int ACCESSORY_STRING_MANUFACTURER ;
+extern const int ACCESSORY_STRING_MODEL;
+extern const int ACCESSORY_STRING_DESCRIPTION;
+extern const int ACCESSORY_STRING_VERSION;
+extern const int ACCESSORY_STRING_URI;
+extern const int ACCESSORY_STRING_SERIAL;
+extern const int ACCESSORY_GET_PROTOCOL;
+extern const int ACCESSORY_SEND_STRING;
+extern const int ACCESSORY_START;
 
-const char *ACCESSORY_MANUFACTURER_NAME = "Android";
-const char *ACCESSORY_MODEL_NAME = "Android Open Automotive Protocol";
-const char *ACCESSORY_DESCRIPTION = "Android Open Automotive Protocol";
-const char *ACCESSORY_VERSION = "1.0";
-const char *ACCESSORY_URI = "http://www.android.com/";
-const char *ACCESSORY_SERIAL_NUMBER = "0000000012345678";
+extern const char *ACCESSORY_MANUFACTURER_NAME;
+extern const char *ACCESSORY_MODEL_NAME;
+extern const char *ACCESSORY_DESCRIPTION;
+extern const char *ACCESSORY_VERSION;
+extern const char *ACCESSORY_URI;
+extern const char *ACCESSORY_SERIAL_NUMBER;
 
-const int HOST_TO_DEVICE_TYPE = 0xc0;
-const int DEVICE_TO_HOST_TYPE = 0x40;
+extern const int HOST_TO_DEVICE_TYPE;
+extern const int DEVICE_TO_HOST_TYPE;
